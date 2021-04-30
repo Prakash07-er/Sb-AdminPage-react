eadb08311d0ac16236179a6d2b2306a0abff0686
Prakash07-er <prakash.u.er@gmail.com>
2021-04-30 13:54:05 +0530

    reactapp

--- src/Dashboard/ilustration.js
+++ src/Dashboard/ilustration.js
@@ -10,10 +10,10 @@ export default function Illustration (){
                                 src="img/undraw_posting_photo.svg" alt="..."/>
                         </div>
                         <p>Add some quality, svg illustrations to your project courtesy of<a
-                                target="_blank"  rel="nofollow" href="https://undraw.co/">unDraw</a>, a
+                                 rel="nofollow" href="https://undraw.co/">unDraw</a>, a
                             constantly updated collection of beautiful svg images that you can use
                             completely free and without attribution!</p>
-                       <a target="_blank"  href="index.html">Browse Illustrations on
+                       <a  href="index.html">Browse Illustrations on
                             unDraw &rarr;</a>
                     </div>
                 </div>
